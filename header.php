e8db1aa6d10929385f4922b597863adfd34c7e0d
Josephine Hall <josephinehall@gmail.com>
2015-07-21 08:40:20 +1000

    Add Bitter google font

--- header.php
+++ header.php
@@ -15,16 +15,16 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
+<link href='http://fonts.googleapis.com/css?family=Bitter:400,700,400italic' rel='stylesheet' type='text/css'>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<a class="skip-link screen-reader-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'sela' ); ?>"><?php _e( 'Skip to content', 'sela' ); ?></a>
-
 			<div class="site-branding">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<img src="/wp-content/uploads/2015/04/logo2.png" width="1180" height="206" alt="Women's Legal Centre ACT and region" />
+					<img src="<?php echo home_url(); ?>/wp-content/themes/sela-child/wlc-logo.png" alt="Women's Legal Centre ACT and region" />
 				</a>
 				<?php sela_the_site_logo(); ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
